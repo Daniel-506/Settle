25dec574bb843f1ad87b9714d4dd9817ef8f22eb
Daniel Mamnuni <daniel@mac.home>
2026-06-06 01:14:56 -0400

    block ws node modules with polyfills

--- metro.config.js
+++ metro.config.js
@@ -3,23 +3,29 @@ const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === "stream") {
-    return { filePath: require.resolve("readable-stream"), type: "sourceFile" };
-  }
-  if (moduleName === "zlib") {
-    return { filePath: require.resolve("browserify-zlib"), type: "sourceFile" };
-  }
-  if (moduleName === "path") {
-    return { filePath: require.resolve("path-browserify"), type: "sourceFile" };
-  }
-  if (moduleName === "http") {
-    return { filePath: require.resolve("stream-http"), type: "sourceFile" };
-  }
-  if (moduleName === "https") {
-    return {
-      filePath: require.resolve("https-browserify"),
-      type: "sourceFile",
-    };
+  const nodeModuleMap = {
+    stream: "readable-stream",
+    zlib: "browserify-zlib",
+    path: "path-browserify",
+    http: "stream-http",
+    https: "https-browserify",
+    crypto: "react-native-crypto-js",
+    util: "util",
+    ws: "react-native",
+  };
+
+  if (nodeModuleMap[moduleName]) {
+    if (moduleName === "ws") {
+      return context.resolveRequest(context, "react-native", platform);
+    }
+    try {
+      return {
+        filePath: require.resolve(nodeModuleMap[moduleName]),
+        type: "sourceFile",
+      };
+    } catch {
+      return context.resolveRequest(context, moduleName, platform);
+    }
   }
   return context.resolveRequest(context, moduleName, platform);
 };
